7e64a01a0f774c5b303644d18fed446f6bef6987
Johnk-Mj <johnkatembue4@gmail.com>
2024-01-25 13:01:30 +0200

    Refactor tab trigger and tab panel attributes to use data-state instead of aria-selected to avoid accessibility issue
    This change updates the tab trigger attributes from "aria-selected" to
    "data-state" in order to track the state of the triggers accurately. The
    "data-state" attribute now stores the values "active" and "inactive" to
    indicate the active and inactive states, respectively. This modification
    allows for more efficient and precise management of the tab triggers.
    Aria-selected has been replaced for accessibility since button or tabs panel are not supporting aria-selected attribute.
    
    Issue: #11

--- packages/tabs/src/const.ts
+++ packages/tabs/src/const.ts
@@ -1,5 +1,7 @@
 export const STATE_TO_TRUE = "true";
 export const STATE_TO_FALSE = "false";
+export const INACTIVE_STATE = "inactive"
+export const ACTIVE_STATE = "active"
 export const DEFAULT_ORIENTATION = "horizontal";
 export const VERTICAL_ORIENTATION = "vertical";
 export const TRANSFORM_DURATION = 600;
