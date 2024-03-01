8a5032e6a9541d5a7b0d61db6e06422de1040094
Johnk-Mj <johnkatembue4@gmail.com>
2024-03-01 18:15:37 +0200

    feat: add vercel integretion

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,13 +3,13 @@ import UnoCSS from "unocss/astro";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-    }),
-    preact(), 
-    mdx()
-  ]
+  integrations: [UnoCSS({
+    injectReset: true
+  }), preact(), mdx()],
+  output: "server",
+  adapter: vercel()
 });
