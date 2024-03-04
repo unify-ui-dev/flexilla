d2753ac2204776348782943fcc17b8050e117e48
Johnk-Mj <johnkatembue4@gmail.com>
2024-03-04 19:29:33 +0200

    feat: update astro config & fix doc tabs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,11 +2,28 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from "unocss/astro";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
+import {
+  transformerNotationDiff
+} from 'shikiji-transformers'
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/docs': {
+      status: 302,
+      destination: '/docs/getting-started'
+    }
+  },
+  markdown: {
+    shikiConfig:{
+      theme:"css-variables",
+      transformers:[
+        transformerNotationDiff(),
+      ]
+    }
+  },
   integrations: [UnoCSS({
     injectReset: true
   }), preact(), mdx()],
