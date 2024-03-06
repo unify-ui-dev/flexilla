ae3677209b2c86f93feb09344d7c8a962c47497a
Johnk-Mj <johnkatembue4@gmail.com>
2024-03-06 13:11:11 +0200

    feat: improve uno-preset
    Match  peer, group and where with fx-*

--- packages/uno-preset/src/preset-helper.ts
+++ packages/uno-preset/src/preset-helper.ts
@@ -14,7 +14,7 @@ export type PrimitivesOptions = {
      */
     selector?: string;
     isAttrBoolean?: boolean;
-}
+};
 
 export const presetVariants = (options: PrimitivesOptions = {}): Variant => {
     const {
@@ -27,21 +27,36 @@ export const presetVariants = (options: PrimitivesOptions = {}): Variant => {
     return {
         name: 'unify-ui-variant',
         match: (matcher: string) => {
-            const regex = new RegExp(`^${prefix}(-not)?-(${variants})[:-]`);
+            const regex = new RegExp(`^(peer-|group-|where-)?${prefix}(-not)?-(${variants})[:-]`);
             const match = matcher.match(regex);
 
             if (!match) return matcher;
+
             const attrGen = !isAttrBoolean
-                ? `[${selector}~='${match[2]}']`
-                : `[${selector}-${match[2]}]`;
+                ? `[${selector}~='${match[3]}']`
+                : `[${selector}-${match[3]}]`;
 
             return {
                 matcher: matcher.slice(match[0].length),
                 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-                selector: (s: any) => (match[1] === '-not')
-                    ? `${s}[${selector}]:not(${attrGen}),:where([${selector}]:not(${attrGen})) ${s}:not(${selector})`
-                    : `${s}${attrGen},:where(${attrGen}) ${s}`,
+                selector: (s: any) => {
+                    const beforePrefixSelector = match[1] ? `${match[1]}` : '';
+                    const preSelector = beforePrefixSelector === "peer-" ?
+                        `.peer${attrGen} ~` : beforePrefixSelector === "group-" ? `.group${attrGen} ` : ""
+                    return (match[2] === '-not')
+                        ? (
+                            beforePrefixSelector === "where-" ? `:where([${selector}]:not(${attrGen})) ${s}:not(${selector})` :
+                                (beforePrefixSelector === "peer-" ?
+                                    `.peer:not(${attrGen}) ~ ${s}` : 
+                                    beforePrefixSelector === "group-" ? 
+                                    `.group:not(${attrGen}) ${s}` : `${s}:not(${attrGen})`)
+                        )
+                        : (
+                            beforePrefixSelector === "where-" ? `:where(${attrGen}) ${s}` :
+                                `${preSelector}${s}${preSelector !== "" ? "" : `${attrGen}`}`
+                        );
+                },
             };
         },
     };
-}
+};
