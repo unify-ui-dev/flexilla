3c2f82c209105105ef19c4ee7115f0264e4da764
Johnk-Mj <johnkatembue4@gmail.com>
2024-09-28 11:36:01 +0200

    feat: improve tailwind plugin and uno preset

--- packages/uno-preset/src/index.ts
+++ packages/uno-preset/src/index.ts
@@ -7,7 +7,7 @@ type MyPresetOptions = {
 
 export default function unifyUI(options: MyPresetOptions = { prefix: "fx" }): Preset {
     return {
-        name: 'unifyui-preset',
+        name: '@flexilla/uno-preset',
         variants: [
             presetVariants({
                 prefix: options.prefix
@@ -16,16 +16,22 @@ export default function unifyUI(options: MyPresetOptions = { prefix: "fx" }): Pr
         shortcuts: {
             'ui-tabs-indicator': 'absolute transform-origin-[0_0] w-[var(--un-tab-indicator-width)] h-[var(--un-tab-indicator-height)] top-[var(--un-tab-indicator-top)] left-[var(--un-tab-indicator-left)]',
             'ui-animated-modal-content': 'animate-[var(--un-modal-animation)] animate-fill-both',
-            'ui-animated-tab-panel':'animate-[var(--un-tab-show-animation)] animate-fill-both',
+            'ui-animated-tab-panel': 'animate-[var(--un-tab-show-animation)] animate-fill-both',
             'ui-overlay': 'fixed inset-0'
         },
         rules: [
             [
                 'ui-popper', {
-                    position: "absolute",
+                    position: "fixed",
                     left: "var(--fx-popper-placement-x)",
                     top: "var(--fx-popper-placement-y)"
                 }
+            ], [
+                'ui-popper-absolute', {
+                    position: "absolute",
+                    left: "var(--fx-popper-placement-x)",
+                    top: "var(--fx-popper-placement-y)"
+                },
             ]
         ]
     }
