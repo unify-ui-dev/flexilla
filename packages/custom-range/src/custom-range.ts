b6428ca3021cadfa75ad363aabecd071e752cea4
sujal402 <sujalkhakhi402@gmail.com>
2024-10-15 02:50:57 +0530

    Chore : Custom-range

--- packages/custom-range/src/custom-range.ts
+++ packages/custom-range/src/custom-range.ts
@@ -1,66 +1,50 @@
 class CustomRangeIndicator {
-    private rangeContainer: HTMLElement
-    private rangeElement: HTMLInputElement
-    private indicatorElement: HTMLSpanElement
-    private indicatorClassname: string[]
+    private rangeContainer: HTMLElement;
+    private rangeElement: HTMLInputElement;
+    private indicatorElement: HTMLSpanElement;
+    private indicatorClassname: string[];
 
     constructor(containerElement_: string | HTMLElement, rangeIndicator?: string) {
-        const containerElement = typeof containerElement_ === "string" ? document.querySelector(`${containerElement_}`) : containerElement_
-        if (!(containerElement instanceof HTMLElement)) throw new Error("No valide container element")
+        const containerElement = typeof containerElement_ === "string" ? document.querySelector(containerElement_) : containerElement_;
+        if (!(containerElement instanceof HTMLElement)) throw new Error("No valid container element");
 
-        this.rangeContainer = containerElement
-        this.rangeElement = this.rangeContainer.querySelector("[data-input-range]") as HTMLInputElement
+        this.rangeContainer = containerElement;
+        this.rangeElement = this.rangeContainer.querySelector("[data-input-range]") as HTMLInputElement;
 
-        if (!(this.rangeElement instanceof HTMLInputElement)) throw new Error("The provided element doesn't have a valid HTMLInputElement. Make sure to add data-input-range to the input element")
+        if (!(this.rangeElement instanceof HTMLInputElement)) throw new Error("The provided element doesn't have a valid HTMLInputElement. Make sure to add data-input-range to the input element");
 
-        this.indicatorClassname = rangeIndicator?.split(" ") || (this.rangeContainer.dataset.rangeIndicator)?.split(" ") || []
-        this.indicatorElement = this.initIndicator()
-        this.initRange()
-        this.updateIndicatorSize()
+        this.indicatorClassname = rangeIndicator?.split(" ") || (this.rangeContainer.dataset.rangeIndicator)?.split(" ") || [];
+        this.indicatorElement = this.initIndicator();
+        this.initRange();
+        this.updateIndicatorSize();
     }
 
     private initIndicator() {
-        const indicator = document.createElement("span")
-        indicator.style.position = "absolute"
-        indicator.style.pointerEvents = "none"
-        if (this.indicatorClassname.length > 0) {
-            indicator.classList.add(...this.indicatorClassname)
-            this.rangeContainer.append(indicator)
-        }
-        return indicator
+        const indicator = document.createElement("span");
+        indicator.classList.add("range-indicator"); 
+        this.rangeContainer.append(indicator);
+        return indicator;
     }
 
-
     private updateIndicatorSize = () => {
-        let size = '0px';
-        const size_ =
+        const sizePercentage =
             ((parseFloat(this.rangeElement.value) - parseFloat(this.rangeElement.min)) /
-                (parseFloat(this.rangeElement.max) - parseFloat(this.rangeElement.min))) *
-            100;
+                (parseFloat(this.rangeElement.max) - parseFloat(this.rangeElement.min))) * 100;
 
-        if (size_ < 14 && size_ > 0) {
-            size = `${size_ + 20 / 100}%`;
-            console.log(size);
-        } else {
-            size = `${size_}%`;
-        }
-
-        this.indicatorElement.style.width = `${size}`;
+        this.indicatorElement.style.width = `${Math.min(Math.max(sizePercentage + 20, 20), 100)}%`; 
+        this.indicatorElement.style.left = `calc(${sizePercentage}% - 10px)`; 
     }
 
     private initRange() {
-        this.rangeElement.addEventListener("input", this.updateIndicatorSize)
+        this.rangeElement.addEventListener("input", this.updateIndicatorSize);
     }
 
-    /**
-     * auto init the Custom Input Range based on the selector provided
-     * @param selector {string} default is [data-fx-custom-range]
-     */
     static autoInit = (selector = "[data-fx-custom-range]") => {
-        const inputRanges = Array.from(document.querySelectorAll(selector)) as HTMLInputElement[]
-        for (const inputRange of inputRanges) new CustomRangeIndicator(inputRange)
+        const inputRanges = Array.from(document.querySelectorAll(selector)) as HTMLInputElement[];
+        for (const inputRange of inputRanges) new CustomRangeIndicator(inputRange);
     }
-    static init = (containerElement_: string | HTMLElement, rangeIndicator?: string) => new CustomRangeIndicator(containerElement_, rangeIndicator)
+
+    static init = (containerElement_: string | HTMLElement, rangeIndicator?: string) => new CustomRangeIndicator(containerElement_, rangeIndicator);
 }
 
-export default CustomRangeIndicator
+export default CustomRangeIndicator;
